1d7b17a7cec1a4e1e3581fd9e02a44d527a1764d
Francis Ramirez <Francis Ramirez@LAPTOP-DF01N7UN>
2024-07-20 16:35:38 -0400

    practica suma.

---
+++ myFirstApp/myFirstApp/Program.cs
@@ -0,0 +1,6 @@
+﻿using myFirstApp.Operaciones;
+
+Operacion operacion = new Operacion();
+
+operacion.SumarNumeros();
+operacion.MultiplicarNumeros();
---
+++ myFirstApp/myFirstApp2/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace myFirstApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           SumarNumeros();
+        }
+        static void SumarNumeros() 
+        {
+            int num1 = 0;
+            int num2 = 0;
+            int suma = 0;
+
+            Console.WriteLine("Digite el num 1: ");
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Digite el num 2: ");
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            suma = (num1 + num2);
+
+            Console.WriteLine($"El resultado es: {suma}");
+
+            Console.ReadLine();
+        }
+
+        static void MultiplicarNumeros() 
+        {
+            int num1 = 0;
+            int num2 = 0;
+            int suma = 0;
+
+            Console.WriteLine("Digite el num 1: ");
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Digite el num 2: ");
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            suma = (num1 * num2);
+
+            Console.WriteLine($"El resultado es: {suma}");
+
+            Console.ReadLine();
+
+        }
+
+    }
+}
